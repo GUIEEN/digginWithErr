8bc6b992a99273f7dd692280b75a46c506054e4b
guieenoutis <guieenoutis@gmail.com>
2018-07-02 23:32:39 +0900

    add Search Component

--- react_practice/hackernews/src/App.js
+++ react_practice/hackernews/src/App.js
@@ -1,21 +1,76 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
 
 class App extends Component {
-  render() {
+  render () {
+    const helloWorld = 'Welcome to the Road to learn React'
+    const user = {first: 'Seung', last: 'Kwak'}
+    console.log(Object.keys(user))
+    var arr = Object.keys(user).map(function (i) {
+      return user[i]
+    })
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className='App'>
+        <h2>{helloWorld}</h2> <br />
+        {arr} <br />
+        <Title children='qwer' />
+        <Title>Hello world</Title> <br />
+        {user.first}
+        <Search />
       </div>
-    );
+    )
   }
 }
 
-export default App;
+// class Search extends Component {
+//   constructor (props) {
+//     super(props)
+
+//     state = {
+//       searchTerm: ''
+//     }
+//   }
+//   searchUpdated (term) {
+//     this.setState({ searchTerm: event.target.value })
+//   }
+// }
+
+class Search extends Component {
+  state = {
+    query: '',
+  }
+ 
+  handleInputChange = () => {
+    this.setState({
+      query: this.search.value
+    })
+  }
+ 
+  render() {
+    return (
+      <form>
+        <input
+          placeholder="Search for..."
+          ref={input => this.search = input}
+          onChange={this.handleInputChange}
+        />
+        <p>{this.state.query}</p>
+      </form>
+    )
+  }
+}
+
+function Title (props) {
+  return (
+    <h1 className='lots-of-styles-here'>
+      <strong>
+        <i className='something-else'>
+          {console.log(props)}
+          { props.children }
+        </i>
+      </strong>
+    </h1>
+  )
+}
+
+export default App
